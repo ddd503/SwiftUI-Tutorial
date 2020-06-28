a25f4214073cf1139cea1d08e33c10408737f599
Dai Kawahara <skbsporta7@gmail.com>
2020-06-28 20:08:57 +0900

    add section2-6

--- SwiftUI-Tutorial/LandmarkList.swift
+++ SwiftUI-Tutorial/LandmarkList.swift
@@ -10,14 +10,26 @@ import SwiftUI
 
 struct LandmarkList: View {
     var body: some View {
-        List(landmarkData) { (landmark) in
-            LandmarkRow(landmark: landmark)
+        NavigationView { // embed in navigation
+            List(landmarkData) { (landmark) in
+                NavigationLink(destination: LandmarkDetail()) { // 遷移先のView指定
+                    LandmarkRow(landmark: landmark) // 表示セル内容
+                }
+                .navigationBarTitle(Text("Landmarks"))
+            }
         }
     }
 }
 
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
-        LandmarkList()
+        Group { // 複数端末でpreview見る
+            LandmarkList()
+                .previewDevice(PreviewDevice(rawValue: "iPhone SE"))
+                .previewDisplayName("iPhone SE")
+            LandmarkList()
+                .previewDevice(PreviewDevice(rawValue: "iPhone XS Max"))
+                .previewDisplayName("iPhone XS Max")
+        }
     }
 }
